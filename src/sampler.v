fa078b3fb51f393101863e4e02d37540bed40996
Sebastian Pfeiler <seb.pfeiler@gmail.com>
2025-03-09 10:33:44 +0100

    Migrate code over

---
+++ src/sampler.v
@@ -0,0 +1,24 @@
+`default_nettype none
+`timescale 10ns/1ns
+
+module sampler(
+	input clock,
+	input reset,
+	input signal,
+	output sampled_signal,
+
+	output falling,
+	output rising
+);
+	reg [1:0] sample_register;
+	always @(posedge clock)
+	if(reset)
+		sample_register <= {signal, signal};
+	else
+		sample_register <= {sample_register[0], signal};
+
+	assign sampled_signal = sample_register[1];
+	assign falling =  sample_register[1] & !sample_register[0];
+	assign rising  = !sample_register[1] &  sample_register[0];
+
+endmodule
--- src/spi_fpu.v
+++ src/spi_fpu.v
@@ -1,83 +1,167 @@
 `default_nettype none
+`timescale 1ns/1ns
 
-
+// icetime says max of 3 MHz, which is fine
 
 module spi_fpu(
-	input clock,
 	input reset,
-
-	input  SPI_clock,
-	input  SPI_in,
-	input  SPI_not_chip_select,
-	output SPI_out
+	input clock,
+	input SPI_clock,
+	input SPI_in,
+	output SPI_out,
+	input SPI_not_chip_select
 );
-	reg [31:0] float_registers[4];
-	
-	wire in_data_valid;
-	wire [7:0] in_data;
-	wire out_data_valid, out_data_ready;
-	wire [7:0] out_data;
+	/* verilator lint_off UNUSED */
+	wire [7:0] SPI_input_data;
+	wire       SPI_input_data_valid;
+
+	wire [7:0] SPI_output_data;
+	wire       SPI_output_data_valid;
+	wire       SPI_output_data_ready;
+	wire       SPI_active;
+	/* verilator lint_on UNUSED */
 	spi_rx spi(
 		.clock(clock),
 		.reset(reset),
-		.SPI_clock(SPI_clock),
 		.SPI_in(SPI_in),
 		.SPI_out(SPI_out),
+		.SPI_clock(SPI_clock),
 		.SPI_not_chip_select(SPI_not_chip_select),
+
+		.in_data_valid(SPI_input_data_valid),
+		.in_data(SPI_input_data),
+
+		.out_data_valid(SPI_output_data_valid),
+		.out_data(SPI_output_data),
+		.out_data_ready(SPI_output_data_ready),
+		.active(SPI_active)
 	);
 
-	localparam COMMAND_IO  = 0;
-	localparam COMMAND_ADD = 1;
-	
-	localparam STATE_idle    = 0;
-	localparam STATE_io      = 1;
-	localparam STATE_compute = 2;
-	localparam STATE_done    = 3;
-	reg [1:0] state;
-	reg [1:0] command;
-	reg [1:0] reg_a, reg_b, reg_result;
-	reg [2:0] in_byte_count;
-	reg [1:0] out_byte_count;
+	wire        float_result_ready;
+	wire [31:0] float_result;
+	reg  [31:0] float_registers[4] /*verilator public */;
+
+	localparam COMMAND_WRITE_TO_REG  = 2'h00;
+	localparam COMMAND_PERFORM_ADD   = 2'h01;
+	localparam COMMAND_READ_FROM_REG = 2'h02;
+
+
+
+	localparam STATE_IDLE               = 0;
+	localparam STATE_READ_COMMAND       = 1;
+	localparam STATE_IO_READ_TARGET_REG = 2;
+	localparam STATE_IO                 = 3;
+	localparam STATE_COMPUTE_READ_REGS  = 4;
+	localparam STATE_COMPUTE_WAIT_FOR   = 5;
+	localparam STATE_WAIT_FOR_END       = 6;
+
+	reg [2:0] state;
+	reg [1:0] read_command;
+	reg [1:0] byte_counter;
+	reg [1:0] register_id_result;
+	reg [1:0] register_id_input_a;
+	reg [1:0] register_id_input_b;
+
 	always @(posedge clock)
 	if(reset)
 	begin
-		state <= STATE_idle;
-		command <= COMMAND_IO;
-		reg_a <= 0;
-		reg_b <= 0;
-		reg_result <= 0;
-		in_byte_count  <= 0;
-		out_byte_count <= 0;
-	end else case(state)
-	STATE_idle:
-		if(in_data_valid)
+		state               <= STATE_IDLE;
+		register_id_result  <= 0;
+		register_id_input_a <= 0;
+		register_id_input_b <= 0;
+		float_registers[0] <= 0;
+		float_registers[1] <= 0;
+		float_registers[2] <= 0;
+		float_registers[3] <= 0;
+	end else if(!SPI_active)
+		state <= STATE_IDLE;
+	else case(state)
+	STATE_IDLE: 
+	begin
+		if(SPI_active)
+			state <= STATE_READ_COMMAND;
+	end
+	STATE_READ_COMMAND:
+	begin
+		if(SPI_input_data_valid)
 		begin
-			command     <= in_data[7:6];
-			reg_a       <= in_data[5:4];
-			reg_b       <= in_data[3:2];
-			reg_result  <= in_data[1:0];
-			state <= command == COMMAND_IO ? STATE_io : STATE_compute;
+			byte_counter <= 0;
+			read_command[1:0] <= SPI_input_data[1:0];
+			case(SPI_input_data[1:0])
+			COMMAND_WRITE_TO_REG : state <= STATE_IO_READ_TARGET_REG;
+			COMMAND_PERFORM_ADD  : state <= STATE_COMPUTE_READ_REGS;
+			COMMAND_READ_FROM_REG: state <= STATE_IO_READ_TARGET_REG;
+			default: state <= STATE_WAIT_FOR_END;
+			endcase
 		end
-	STATE_io:
-		if(out_data_valid & out_data_ready)
-			out_byte_count <= out_byte_count + 1;
-		if(in_data_valid)
+	end
+	STATE_IO_READ_TARGET_REG:
+	begin
+		if(SPI_input_data_valid)
 		begin
-			in_byte_count <= in_byte_count + 1;
-			float_registers[in_byte_count[2] == 0 ? reg_a : reg_b][8 * in_byte_count[1:0] +: 8] <= in_data;
+			register_id_result <= SPI_input_data[1:0];
+			state <= STATE_IO;
 		end
-	STATE_compute:
-		case(command)
-		COMMAND_ADD: float_registers[reg_result] <= float_add_result;
-		default: begin end
+	end
+	STATE_IO:
+	begin
+		case(read_command)
+		COMMAND_WRITE_TO_REG:
+			if(SPI_input_data_valid)
+			begin
+				float_registers[register_id_result][byte_counter * 8 +: 8] <= SPI_input_data;
+				byte_counter <= byte_counter + 1;
+
+				if(byte_counter == 2'd3)  state <= STATE_WAIT_FOR_END;
+			end
+		COMMAND_READ_FROM_REG:
+			if(SPI_output_data_ready)
+			begin
+				byte_counter <= byte_counter + 1;
+				if(byte_counter == 2'd3)  state <= STATE_WAIT_FOR_END;
+			end
+		default: state <= STATE_WAIT_FOR_END;
 		endcase
-	STATE_done:
-		in_byte_count <= 0;
-		out_byte_count <= 0;
-		if(SPI_not_chip_select)
-			state <= STATE_idle;
+	end
+	STATE_COMPUTE_READ_REGS:
+	begin
+		if(SPI_input_data_valid)
+		begin
+			case(byte_counter)
+			2'h0: register_id_input_a <= SPI_input_data[1:0];
+			2'h1: register_id_input_b <= SPI_input_data[1:0];
+			2'h2:
+			begin
+				register_id_result  <= SPI_input_data[1:0];
+				state <= STATE_COMPUTE_WAIT_FOR;
+			end
+			default: state <= STATE_WAIT_FOR_END;
+			endcase
+			byte_counter <= byte_counter + 1;
+		end
+	end
+	STATE_COMPUTE_WAIT_FOR:
+		if(float_result_ready)
+		begin
+			float_registers[register_id_result] <= float_result;
+			state <= STATE_WAIT_FOR_END;
+		end
+	STATE_WAIT_FOR_END:
+	begin
+	end
+
 	endcase
-	
-	assing out_data = float_registers[reg_result][byte_count[1:0]];
-	
+	assign SPI_output_data       = state == STATE_IO ? float_registers[register_id_result][byte_counter * 8 +: 8] : 8'b0;
+	assign SPI_output_data_valid = state == STATE_IO && read_command == COMMAND_READ_FROM_REG;
+
+	float_adder_v2 adder(
+		.clock(clock),
+		.reset(reset),
+		.in_valid(state == STATE_COMPUTE_WAIT_FOR),
+		.in_a(float_registers[register_id_input_a]),
+		.in_b(float_registers[register_id_input_b]),
+		.out(float_result),
+		.out_valid(float_result_ready),
+		.out_reference(32'b0)
+	);
 endmodule
